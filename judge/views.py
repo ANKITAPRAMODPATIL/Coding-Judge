6a3b0d6fc2b5e14b9295b80492344de8f6d467b3
Ankita Patil <ankitappatil421@gmail.com>
2026-08-29 17:13:01 +0530

    update view file

--- judge/views.py
+++ judge/views.py
@@ -258,7 +258,7 @@ def submission_history(request):
 @login_required(login_url='/login/')
 def get_ai_hint(request, problem_id):
     try:
-        client = genai.Client(api_key="AQ.Ab8RN6K5yU3LXsLNESqKQ9dJ1eVeFQ-FRV8IW7hJ_khj8pw7JQ")
+        client = genai.Client(api_key="")
         # print("DEBUG KEY VALUE:", repr(settings.GEMINI_API_KEY), flush=True)
         # print("DEBUG KEY LENGTH:", len(settings.GEMINI_API_KEY), flush=True)
         
@@ -322,7 +322,7 @@ Problem Description:
 @login_required(login_url='/login/')
 def review_code(request, problem_id):
     try:
-        client = genai.Client(api_key="AQ.Ab8RN6K5yU3LXsLNESqKQ9dJ1eVeFQ-FRV8IW7hJ_khj8pw7JQ")
+        client = genai.Client(api_key="")
         #print("DEBUG KEY VALUE:", repr(settings.GEMINI_API_KEY), flush=True)
         #print("DEBUG KEY LENGTH:", len(settings.GEMINI_API_KEY), flush=True)
        
